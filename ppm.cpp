6b4701093a9314d7ab3eafd8fc3647dcf10c5524
David-Vella <davidfvellajr@gmail.com>
2020-05-17 19:00:50 -0400

    Added comments and using stdint

--- ppm.cpp
+++ ppm.cpp
@@ -1,73 +1,59 @@
+/*
+ * Class for decoding ppm signals sent from the RF receiver
+ * using interrupts
+ */ 
+
 #include "ppm.h"
 
 
-ppmDecoder::ppmDecoder() :
-    is_synced(true), 
-    is_connected(true),
-    sync_ptr(data), 
-    chan_ptr(data) { }
+ppmDecoder::ppmDecoder() : sync_ptr(data), chan_ptr(data) { }
 
-void ppmDecoder::sync() {
-    for (int i = 0; i < MAXIMUM_RESYNC_ATTEMPTS; ++i) {
-
-        if (*sync_ptr > MINIMUM_SYNC_PULSEWIDTH) {
-            is_synced = true;
+// Checks if the sync_ptr is pointed to the sync channel in the 
+// data array. If not, the data array is traversed until the 
+// sync channel is found. MAXIMUM_RESYNC_ATTEMPTS limits the 
+// number of complete array traverses.
+void ppmDecoder::sync() 
+{
+    for (uint8_t i = 0; i < MAXIMUM_RESYNC_ATTEMPTS; ++i) 
+    {
+        if (*sync_ptr > MINIMUM_SYNC_PULSEWIDTH) 
             break;
-        }
 
-        if (sync_ptr == data + 13) sync_ptr = data;
-        else ++sync_ptr;
-
-        is_synced = false;
+        if (sync_ptr == data + PPM_DATA_ARRAY_SIZE - 1)
+            sync_ptr = data;
+        else
+            ++sync_ptr;
     }
-
-    #ifdef TELEMETRY_CHECKING
-    static int count = 0;
-
-    static int prev = *sync_ptr;
-
-    if (*sync_ptr - prev < 10) ++count;
-    else count = 0;
-
-    prev = *sync_ptr;
-
-    if (count > 30) is_connected = false;
-    else is_connected = true;
-    #endif
-
-    #ifdef PPM_PRINT_WARNINGS
-    if (!is_synced) Serial.println("PPM: out of sync");
-    #ifdef TELEMETRY_CHECKING
-    if (!is_connected) Serial.println("PPM: telemetry lost");
-    #endif
-    #endif
 }
 
-int ppmDecoder::get(int chan) {
+// Returns the pulsewidth of the corresponding receiver channel 
+// in milliseconds.
+uint16_t ppmDecoder::get(uint8_t chan) 
+{
     int val;
-    if (sync_ptr + chan <= data + 13) {
-        val = *(sync_ptr + chan);
-    }
-    else {
-        val = *(sync_ptr + chan - 14);
-    }
-    val += 300;
-    if (val < MINIMUM_PPM_PULSEWIDTH) {
+
+    if (sync_ptr + chan <= data + PPM_DATA_ARRAY_SIZE - 1)
+        val = sync_ptr[chan];
+    else 
+        val = sync_ptr[chan - PPM_DATA_ARRAY_SIZE];
+
+    if (val < MINIMUM_PPM_PULSEWIDTH)
         val = MINIMUM_PPM_PULSEWIDTH;
-    }
-    if (val > MAXIMUM_PPM_PULSEWIDTH) {
+
+    if (val > MAXIMUM_PPM_PULSEWIDTH)
         val = MAXIMUM_PPM_PULSEWIDTH;
-    }
-    return val;
+
+    return val + PPM_PWM_OFFSET;
 }
 
-bool ppmDecoder::error() {
-    return !is_connected || !is_synced;
-}
-
-void ppmDecoder::toggle() {
+// Updates the data array. Called by interrupt routine
+void ppmDecoder::toggle() 
+{
     *chan_ptr = micros() - timer;
     timer = micros();
-    if (chan_ptr == data + 13) chan_ptr = data;
-    else ++chan_ptr;
+
+    if (chan_ptr == data + PPM_DATA_ARRAY_SIZE - 1) 
+        chan_ptr = data;
+    else 
+        ++chan_ptr;
 }
--- ppm.h
+++ ppm.h
@@ -1,57 +1,69 @@
+/*
+ * Class for decoding ppm signals sent from the RF receiver
+ * using interrupts
+ */ 
+
 #ifndef PPM_H
 #define PPM_H
 
 #include <Arduino.h>
 
+#define MAXIMUM_PPM_PULSEWIDTH 1700
+#define MINIMUM_PPM_PULSEWIDTH 700
+#define MINIMUM_SYNC_PULSEWIDTH 10000
+#define PPM_PWM_OFFSET 300
+#define MAXIMUM_RESYNC_ATTEMPTS 5
+#define RECEIVER_CHANNELS 6
+#define PPM_DATA_ARRAY_SIZE (RECEIVER_CHANNELS + 1) * 2
 
-// #define PPM_PRINT_WARNINGS
-//#define TELEMETRY_CHECKING
 
-class ppmDecoder {
+class ppmDecoder 
+{
     public:
         ppmDecoder();
+
+        // Checks if the sync_ptr is pointed to the sync channel in the 
+        // data array. If not, the data array is traversed until the 
+        // sync channel is found. MAXIMUM_RESYNC_ATTEMPTS limits the 
+        // number of complete array traverses.
         void sync();
-        int get(int chan);
-        bool error();
+
+        // Returns the pulsewidth of the corresponding receiver channel 
+        // in milliseconds.
+        uint16_t get(uint8_t chan);
+
+        // Receiver channels used to index the data array
+        static constexpr uint8_t ARL = 2;
+        static constexpr uint8_t ELE = 4;
+        static constexpr uint8_t THR = 6;
+        static constexpr uint8_t RUD = 8;
+        static constexpr uint8_t GER = 10;
+        static constexpr uint8_t AUX = 12;
+
+        // Updates the data array. Called by interrupt routine
         void toggle();
 
-        static constexpr int ARL = 2;
-        static constexpr int ELE = 4;
-        static constexpr int THR = 6;
-        static constexpr int RUD = 8;
-        static constexpr int GER = 10;
-        static constexpr int AUX = 12;
-
-        static constexpr int MAXIMUM_PPM_PULSEWIDTH = 2000;
-        static constexpr int MINIMUM_PPM_PULSEWIDTH = 1000;
-        static constexpr int MINIMUM_SYNC_PULSEWIDTH = 10000;
-        static constexpr int MAXIMUM_RESYNC_ATTEMPTS = 5;
-
     private:
-        bool is_synced;
-        bool is_connected;
+        uint16_t data[PPM_DATA_ARRAY_SIZE];
+
+        uint16_t* chan_ptr;
+        uint16_t* sync_ptr;
 
         unsigned long timer;
-        int data[14];
-        int* chan_ptr;
-        int* sync_ptr;
 };
 
+// Associates an instance of the ppmDecoder class with a pin.
+// Macro defined so compiler can deduce instance of ppmDecoder class
 #define assignPpmDecoderToPin(handler, pin) \
     pinMode(pin, INPUT_PULLUP); \
     attachInterrupt( \
         digitalPinToInterrupt(pin), \
-        [](void){ \
-            handler.toggle(); \
-        }, \
+        [](void){ handler.toggle(); }, \
         CHANGE \
     );
 
+// Disassociates an instance of the ppmDecoder class with a pin
 #define removePpmDecoderFromPin(handler, pin) \
     detachInterrupt(pin);
-
-#ifdef PPM_PRINT_WARNINGS
-#define SERIAL_CONNECTION
-#endif
 
 #endif
