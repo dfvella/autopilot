5ffcdb08f5d8463ef2a65774289da90bd33dd091
David-Vella <davidfvellajr@gmail.com>
2020-05-22 22:21:06 -0400

    Comments and stdint

--- servo.cpp
+++ servo.cpp
@@ -1,11 +1,18 @@
+/*
+ * Class for generating pwm signals to control the servo motors
+ * which deflect control surfaces to change the aircraft's attitude
+ */
+
 #include "servo.h"
 
-Servo::Servo(int pin_in) : pin(pin_in)
+Servo::Servo(uint8_t pin_in) : pin(pin_in)
 {
     pinMode(pin, OUTPUT);
 }
 
-void Servo::set(int signal_in)
+// Sets the pulsewidth in microseconds of the pulse generated
+// by a call to write() or write_all()
+void Servo::set(uint16_t signal_in)
 {
     if (signal_in > MAX_THROW) 
         signal_in = MAX_THROW;
@@ -13,46 +20,56 @@ void Servo::set(int signal_in)
     if (signal_in < MIN_THROW)
         signal_in = MIN_THROW;
 
-    if (abs(signal_in - signal) > 10)
+    if (abs(signal_in - signal) > NOISE_FILTER_THRESHOLD)
         signal = signal_in;
 }
 
-int Servo::get()
+// Returns the pulsewidth in microseconds of the pulse generated
+// by a call to write() or write_all()
+uint16_t Servo::get()
 {
     return signal;
 }
 
+// Writes servo output pin high
 void Servo::high()
 {
     digitalWrite(pin, HIGH);
     timer = micros();    
 }
 
+// Writes servo output pin low
 void Servo::low()
 {
     while (micros() - timer < signal);
     digitalWrite(pin, LOW);    
 }
 
-void Servo::write(int pulseWidth)
+// Generates a pwm signal on the servo output pin with a pulsewidth
+// equal to signal member variable 
+void Servo::write(uint16_t signal_in)
 {
     high();
     low();
 }
 
-void Servo::write_all(Servo* servo[], const int num)
+// Generates pwm signals on the output pins of all the servos in the 
+// array simultaneously 
+void Servo::write_all(Servo* servo[], const uint8_t num)
 {
+    // Make a copt of the servo array
     Servo* sorted_servo[num];
 
-    for (int i = 0; i < num; ++i)
+    for (uint8_t i = 0; i < num; ++i)
         sorted_servo[i] = servo[i];
     
-    int correct = 0;
+    uint8_t correct = 0;
 
+    // Bubble sort the array according to signal pulsewidth
     while (correct != num - 1)
     {
         correct = 0;
-        for (int i = 0; i + 1 != num; ++i)
+        for (uint8_t i = 0; i + 1 != num; ++i)
         {
             if (sorted_servo[i]->signal <= sorted_servo[i + 1]->signal)
                 ++correct;
@@ -64,14 +81,17 @@ void Servo::write_all(Servo* servo[], const int num)
             }
         }
     }
-
-    for (int i = 0; i < num; ++i)
+    
+    // Write all the pins high with padding according to PULSE_PADDING
+    // between rising edges 
+    for (uint8_t i = 0; i < num; ++i)
     {
         sorted_servo[i]->high();
         unsigned long temp = micros();
-        while (micros() - temp < 20);
+        while (micros() - temp < PULSE_PADDING);
     }
 
-    for (int i = 0; i < num; ++i) 
+    // Write all the servo output pins low
+    for (uint8_t i = 0; i < num; ++i) 
         sorted_servo[i]->low();
 }
--- servo.h
+++ servo.h
@@ -1,28 +1,43 @@
+/*
+ * Class for generating pwm signals to control the servo motors
+ * which deflect control surfaces to change the aircraft's attitude
+ */
+
 #ifndef SERVO_H
 #define SERVO_H
 
 #include <Arduino.h>
 
+// Time between writing output pins high in the write_all() 
+// method
+#define PULSE_PADDING 20 // microseconds
+
+// Minimum difference required for the signal property to update
+// upon a call to set() method. For noise reduction.
+#define NOISE_FILTER_THRESHOLD 10 // microseconds
+
+
 class Servo 
 {
     public:
-        Servo(int pin_in);
+        Servo(uint8_t pin_in);
 
-        void set(int signal_in);
-        int get();
+        void set(uint16_t signal_in);
+        uint16_t get();
 
-        void write(int pulsewidth);
-        static void write_all(Servo* servo[], const int num);
+        void write(uint16_t signal_in);
+        static void write_all(Servo* servo[], const uint8_t num);
 
-        static constexpr int MAX_THROW = 1800;
-        static constexpr int MIN_THROW = 1200;
+        static constexpr uint16_t MAX_THROW = 1800;
+        static constexpr uint16_t MIN_THROW = 1200;
+        static constexpr uint16_t CENTER = 1500;
 
     private:
         void high();
         void low();
 
-        int pin;
-        int signal;
+        uint8_t pin;
+        uint16_t signal;
 
         unsigned long timer;
 };
